3ba6ade4c7b91ca688f8ba9ad03f0be699961c22
Himanshu Kumar <himanshusingh11062003@gmail.com>
2026-05-04 11:21:58 +0530

    style: format education addresses to appear on new line

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,9 +1,9 @@
 import Timeline from './Timeline';
 
 const data = [
-  { title: 'B.Tech – Computer Science & Engineering', org: 'Lovely Professional University, Punjab, India', date: 'August 2022 – Present' },
-  { title: 'Intermediate', org: 'Global International School, Muzaffarpur, Bihar', date: 'April 2020 – March 2022' },
-  { title: 'Matriculation', org: 'Delhi Public School International, Muzaffarpur, Bihar', date: 'April 2019 – March 2020' },
+  { title: 'B.Tech – Computer Science & Engineering', org: <>Lovely Professional University,<br />Punjab, India</>, date: 'August 2022 – Present' },
+  { title: 'Intermediate', org: <>Global International School,<br />Muzaffarpur, Bihar</>, date: 'April 2020 – March 2022' },
+  { title: 'Matriculation', org: <>Delhi Public School International,<br />Muzaffarpur, Bihar</>, date: 'April 2019 – March 2020' },
 ];
 
 export default function Education() {
